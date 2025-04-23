b5de3efc6c8c0a1d67cd53faf048a0bf2edf0eb6
carlosd2000 <caerlosdavid29@gmail.com>
2025-04-23 14:53:52 -0500

    instalar plugin dev tools para ver las lineas delos componentes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    cors: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    // Añade esta configuración:
-    allowedHosts: [
-      '9213-45-225-227-89.ngrok-free.app', // Tu dominio ngrok actual
-      '.ngrok-free.app' // Permite cualquier subdominio de ngrok
-    ]
-  }
+  },
 })
