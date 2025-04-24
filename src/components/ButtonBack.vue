ce8c0a0bccac8002cc38e9e5db9052b2d8e9455b
Hermis999 <hermisjulian999@gmail.com>
2025-04-24 09:56:40 -0500

    cambios de aparicion

--- src/components/ButtonBack.vue
+++ src/components/ButtonBack.vue
@@ -1,16 +1,7 @@
 <template>
     <div class="m-0 p-0">
-        <button class="m-1 p-0 border-0 bg-transparent" v-if="!firstPage" @click="$router.push('/')">
+        <button class="m-1 p-0 border-0 bg-transparent"  @click="$router.push('/')">
             <i class="bi bi-arrow-left"></i>
         </button>
     </div>
 </template>
-<script setup>
-import { useRoute} from 'vue-router'
-import { computed } from 'vue'
-
-const route = useRoute()
-
-// Define reactividad para determinar si estamos en la página principal
-const firstPage = computed(() => route.path === '/')
-</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,34 @@
-<script setup>
-import ButtonBack from '../components/ButtonBack.vue'
-</script>
-
 <template>
   <div class="col-12 row p-0 m-0 d-flex justify-content-center align-items-center header-border">
     
-    <div class="col-5 m-0 p-0 d-flex justify-content-center align-items-center">
-      <ButtonBack class="col-3 ml-2 d-flex justify-content-center"/>
-      <p class="col-8 m-0 p-1">hh:mm:ss</p>
+    <div class="col-5 m-0 p-0 d-flex justify-content-start align-items-center">
+      <ButtonBack class="col-3 ml-2 d-flex justify-content-center" v-if="!back"/>
+      <p class="col-8 m-0 p-1 ml-2">hh:mm:ss</p>
     </div>
     <div class="col-7 row p-1 m-0 d-flex justify-content-end align-items-center">
       <button class="btn btn-light border-0 p-0 bg-transparent" @click="$router.push('/')">
         <p class="m-0 p-1">$20,000,000.00</p>
       </button>
       
-      <div class="col-2 p-2 m-0 d-flex justify-content-end align-items-center">
+      <div class="col-2 p-2 m-0 d-flex justify-content-end align-items-center" v-if="bell">
         <i class="bi bi-bell"></i>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import ButtonBack from '../components/ButtonBack.vue'
+
+import { useRoute} from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+// Define reactividad para determinar si estamos en la página principal
+const bell = computed(() => route.path === '/')
+const back = computed(() => route.path === '/')
+</script>
 
 <style scoped>
 .header-border {
