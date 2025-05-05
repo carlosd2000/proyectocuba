bd4b0238dc9e3f3d133704780236d73105fb447c
Hermis999 <hermisjulian999@gmail.com>
2025-05-05 10:00:01 -0500

    subido

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a8o8m10q71o"
+    "revision": "0.sdk705dntdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
