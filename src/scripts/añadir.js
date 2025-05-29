6c6ceb0e2da8e16bccc4e85b471edbe054554ed5
Hermis999 <hermisjulian999@gmail.com>
2025-05-29 11:05:17 -0500

    añadir

--- src/scripts/añadir.js
+++ src/scripts/añadir.js
@@ -5,7 +5,7 @@ import { filasFijas, filasExtra, calcularTotales } from './operaciones';
 import { ref } from 'vue';
 import { obtenerHoraCuba } from './horacuba.js';
 import { obtenerBancoPadre } from './FunctionBancoPadre.js';
-
+import { expandirApuestasIncrementativas } from './operaciones.js'
 
 async function ejemploUso() {
   const bancoId = await obtenerBancoPadre();
@@ -270,13 +270,16 @@ export async function sincronizarPendientes() {
             );
             
             // Verificar si el horario de esta apuesta ya pasó
-            fueraDeTiempo = serverTime.toMillis() > horaCierre.getTime();
+            // fueraDeTiempo = serverTime.toMillis() > horaCierre.getTime();
             
             // Si el horario ya pasó, verificar si fue creada antes del cierre
-            if (fueraDeTiempo && apuesta.creadoEn) {
-              const creadoEn = new Date(apuesta.creadoEn);
-              fueraDeTiempo = creadoEn.getTime() < horaCierre.getTime();
-            }
+            // if (fueraDeTiempo && apuesta.creadoEn) {
+            //   const creadoEn = new Date(apuesta.creadoEn);
+            //   fueraDeTiempo = creadoEn.getTime() < horaCierre.getTime();
+            // }
+
+            fueraDeTiempo = await verificarFueraDeTiempo(apuesta.horario, snap.data(), serverTime);
+            
           }
           
           await setDoc(docRef, {
